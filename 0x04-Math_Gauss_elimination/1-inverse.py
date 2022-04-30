a3db0534cc784a699953505ebecee063ada37945
maurolabella <maurolabella@yahoo.com>
2022-04-30 12:48:59 -0300

    a

---
+++ 0x04-Math_Gauss_elimination/1-inverse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import numpy as np
+# gaussian_elimination = __import__(
+#   '0-gaussian_elimination').gaussian_elimination
+
+if __name__ == "__main__":
+
+    def inverse(A):
+        ainv = np.linalg.inv(A)
+        return ainv
+
+    A = np.zeros((4, 4))
+    A[2, 0] = 1
+    A[0, 1] = 1
+    A[1, 3] = 1
+    A[3, 2] = 1
+    print("The matrix A:\n", A)
+    print("The inverse of the matrix A:\n", inverse(A))
+    print("The identity matrix (The matrix A x The inverse of the matrix A): \n", A @ inverse(A))
