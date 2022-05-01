37109a0e832adbc3d60f9570e0c3d0b5d9f4db77
maurolabella <maurolabella@yahoo.com>
2022-04-30 22:42:02 -0300

    a

--- 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
+++ 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+
+import sys
 import numpy as np
 
 
 def gaussian_elimination(A, b):
 
     try:
-        x = np.linalg.solve(A, b)
-        return x
-    except:
-        print("You can't divide by zero!")
+        if not np.any(b):
+            x = [0, 0, 0]
+            return x
+        else:
+            x = np.linalg.solve(A, b)
+            return x
+    except Exception as e:
+        if e.__class__.__name__ == "LinAlgError":
+            if not np.any(b):
+                x = [0, 0, 0]
+                return x
+            else:
+                print("You can't divide by zero!")
+        else:
+            print(e)
+            print(e.__class__.__name__)
