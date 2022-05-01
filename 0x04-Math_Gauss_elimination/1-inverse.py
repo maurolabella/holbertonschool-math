cd191301829ee9e7acb70b0b0515ec8ed7629c1b
maurolabella <maurolabella@yahoo.com>
2022-05-01 17:02:25 -0300

    a

--- 0x04-Math_Gauss_elimination/1-inverse.py
+++ 0x04-Math_Gauss_elimination/1-inverse.py
@@ -5,16 +5,19 @@ import numpy as np
 #   '0-gaussian_elimination').gaussian_elimination
 
 if __name__ == "__main__":
+	def inverse(A):
+		try:
+			ainv = np.linalg.inv(A)
+			return ainv
+		except np.linalg.LinAlgError:
+			print("You can't divide by zero!")
+			return None
 
-    def inverse(A):
-        ainv = np.linalg.inv(A)
-        return ainv
-
-    A = np.zeros((4, 4))
-    A[2, 0] = 1
-    A[0, 1] = 1
-    A[1, 3] = 1
-    A[3, 2] = 1
-    print("The matrix A:\n", A)
-    print("The inverse of the matrix A:\n", inverse(A))
-    print("The identity matrix (The matrix A x The inverse of the matrix A): \n", A @ inverse(A))
+A = np.zeros((4, 4))
+A[2, 0] = 1
+A[0, 1] = 1
+A[1, 3] = 1
+A[3, 2] = 1
+print("The matrix A:\n", A)
+print("The inverse of the matrix A:\n", inverse(A))
+print("The identity matrix (The matrix A x The inverse of the matrix A): \n", A @ inverse(A))
